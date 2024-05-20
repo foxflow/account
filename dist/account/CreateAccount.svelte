b1aba4125368d714ceffa3278508917c0cda580f
Alvin Engler <alvinengler@gmail.com>
2024-05-20 09:54:26 -0700

    customization props for CreateAccount

--- dist/account/CreateAccount.svelte
+++ dist/account/CreateAccount.svelte
@@ -1,24 +1,41 @@
 <script>
 	import { supabase } from '../supabase';
-	let name = '',
-		slug = '';
+	export let nameLabelText = `New Workspace / Team Name`;
+	export let slugLabelText = `Slug`;
+	export let buttonText = `Create Team`;
+	export let slugHelpText = `<strong>A unique url for your team</strong> &ndash; only contain lowercase letters,
+				numbers, and hyphens are allowed.`;
+	export let onAfterCreate = () => {};
+	export let onError = (err) => {
+		console.error(error);
+	};
+	export let name = ''; // exporting allows passing in a default value
+	export let slug = ''; // ^^ same here
 
 	const submitCreateAccount = async () => {
 		const { data, error } = await supabase.rpc('create_account', {
 			name,
 			slug
 		});
+
+		if (!error) {
+			onAfterCreate(data);
+		} else {
+			onError(error);
+		}
 	};
 </script>
 
 <form on:submit|preventDefault={submitCreateAccount}>
 	<div class="row align-items-end">
 		<div class="col-12 mb-3">
-			<label for="name" class="form-label">New Workspace / Team Name</label>
+			<label for="name" class="form-label">{nameLabelText}</label>
 			<input id="name" required bind:value={name} type="text" class="form-control" />
 		</div>
 		<div class="col-12 mb-3">
-			<label for="slug" class="form-label">Slug</label>
+			<label for="slug" class="form-label">
+				{slugLabelText}
+			</label>
 			<input
 				id="slug"
 				required
@@ -29,10 +46,11 @@
 				class="form-control"
 			/>
 			<div class="help-text fs-5 mt-2 text-muted">
-				This creates unique URL's for your team. It can only contain lowercase letters,
-				numbers, and hyphens.
+				{@html slugHelpText}
 			</div>
 		</div>
 	</div>
-	<button type="submit" class="btn btn-primary mt-3 w-100">Create Team</button>
+	<button type="submit" class="btn btn-primary mt-3 w-100">
+		{buttonText}
+	</button>
 </form>
--- dist/account/CreateAccount.svelte.d.ts
+++ dist/account/CreateAccount.svelte.d.ts
@@ -2,7 +2,14 @@
 /** @typedef {typeof __propDef.events}  CreateAccountEvents */
 /** @typedef {typeof __propDef.slots}  CreateAccountSlots */
 export default class CreateAccount extends SvelteComponent<{
-    [x: string]: never;
+    nameLabelText?: string;
+    slugLabelText?: string;
+    buttonText?: string;
+    slugHelpText?: string;
+    onAfterCreate?: () => void;
+    onError?: (err: any) => void;
+    name?: string;
+    slug?: string;
 }, {
     [evt: string]: CustomEvent<any>;
 }, {}> {
@@ -13,7 +20,14 @@ export type CreateAccountSlots = typeof __propDef.slots;
 import { SvelteComponent } from "svelte";
 declare const __propDef: {
     props: {
-        [x: string]: never;
+        nameLabelText?: string;
+        slugLabelText?: string;
+        buttonText?: string;
+        slugHelpText?: string;
+        onAfterCreate?: () => void;
+        onError?: (err: any) => void;
+        name?: string;
+        slug?: string;
     };
     events: {
         [evt: string]: CustomEvent<any>;
